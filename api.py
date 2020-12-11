05587742124c379c00045a8793ef4b15cc0e95d9
astudillome <projectagility@gmail.com>
2020-12-11 15:58:51 -0700

    sets up backend file structure and app environement

---
+++ api.py
@@ -0,0 +1,17 @@
+import time
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+  return { 'time': time.time()}
+
+@app.route('/nytresults', methods=['GET'])
+def get_articles():
+  data = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=0XvEh8pQ6usIUskmlliZNvlebumtyLml").json()
+  print(data)
+  return {'nyt_data': data}
